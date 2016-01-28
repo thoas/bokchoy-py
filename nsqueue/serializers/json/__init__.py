a3b71c6c32d97596b0eebdc9365e8011de26e4b3
Florent Messa <florent.messa@gmail.com>
2016-01-28 13:07:18 +0100

    Add JSONEncoder for uuid type

--- nsqueue/serializers/json/__init__.py
+++ nsqueue/serializers/json/__init__.py
@@ -6,9 +6,20 @@ except ImportError:
     import json
 
 
+import uuid
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, uuid.UUID):
+            return str(obj)
+
+        return super(JSONEncoder, self).default(obj)
+
+
 class Serializer(base.Serializer):
     def dumps(self, value):
-        return json.dumps(value)
+        return json.dumps(value, cls=JSONEncoder)
 
     def loads(self, value):
         return json.loads(value)
